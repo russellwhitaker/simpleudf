42c94a14bf228e02e63b9216fe36ccb6b8c53e5e
Russell Whitaker <rwhitaker@ulive.com>
2016-12-23 13:29:33 -0800

    Brought Clojure and Hive deps up to date

--- resources/leiningen/new/simpleudf/core.clj
+++ resources/leiningen/new/simpleudf/core.clj
@@ -2,11 +2,13 @@
   (:gen-class))
 
 (defn hello-{{name}}
-  "Good programmers comment their code where necessary."
+  "Good programmers comment their code where necessary.
+   Better programmers comment their code only where necessary."
   [name]
   (str "Greetings of the day " name "!"))
 
 (defn goodbye-{{name}}
-  "Good programmers comment their code where necessary."
+  "Good programmers comment their code where necessary.
+   Better programmers comment their code only where necessary."
   [name]
   (str "Fare thee well " name "!"))
